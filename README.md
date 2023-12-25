d43dbe0d3a0d1707966646ac764958cd1793719e
Philippe Fanaro <philippefanaro@gmail.com>
2023-12-25 11:35:19 -0300

    most websites block cross-site requests (sic)...

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 Exploring web search preview, because I think the current state of previews is very underwheling.
 
+The fact that most websites block cross-site requests pretty much makes this so much more underwhelming than it should be...
+
 ## Tasks
 
 - [ ] Add `<iframe>` to every result to see if it's performant.
+  - The real problem is that most websites don't allow for `<iframe>`s...
   - [ ] Maybe I could extend this to any link on any website, as a popover.
 
 ## Other Web Search Preview Browser Extensions
--- src/content.ts
+++ src/content.ts
@@ -3,7 +3,7 @@ window.onload = () => {
     document.querySelectorAll<HTMLDivElement>("#rso > div")
   )
 
-  resultsDivs.slice(0, 3).forEach((resultDiv) => {
+  resultsDivs.forEach((resultDiv) => {
     const a = resultDiv.querySelector<HTMLAnchorElement>(
       "div > div > div > div > div > span > a"
     )
@@ -13,21 +13,14 @@ window.onload = () => {
 
     const iframe = document.createElement("iframe")
     iframe.src = a!.href
-    iframe.width = "300px"
-    iframe.height = "400px"
-    iframe.style.visibility = "hidden"
+    iframe.width = "600px"
+    iframe.height = "300px"
+    iframe.style.display = "none"
     iframe.style.marginBottom = "20px"
     iframe.style.marginTop = "-10px"
 
-    iframe.onerror = (e) => {
-      console.log("error")
-      console.log(e)
-    }
-    iframe.onload = (e) => {
-      console.log("loaded")
-      console.log(iframe.contentDocument?.location)
-      console.log(e)
-      iframe.style.visibility = "visible"
+    iframe.onload = () => {
+      iframe.style.display = "block"
     }
 
     resultDiv.append(iframe)
